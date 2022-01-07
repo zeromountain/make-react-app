d38ff328101683ee93f9f99fce67ebaacdec0860
zeromountain <sonyeongsan@gmail.com>
2022-01-08 00:38:13 +0900

    검색폼 구현

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,9 +1,25 @@
 const tag = '[Controller]';
-
 export default class Controller {
-  constructor(store, views) {
+  constructor(store, { searchFormView }) {
+    console.log(tag, 'Controller');
     this.store = store;
 
-    // TODO
+    this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+  }
+
+  subscribeViewEvents() {
+    this.searchFormView
+      .on('@submit', (e) => this.search(e.detail))
+      .on('@reset', () => this.reset());
+  }
+
+  search(keyword) {
+    console.log(tag, keyword);
+  }
+
+  reset() {
+    console.log(tag, 'reset');
   }
 }
--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,13 +1,15 @@
 import View from './View.js';
-import { qs } from '../helpers';
+import { on, qs } from '../helpers.js';
+
+const tag = '[SearchFormView]';
 
 export default class SearchFormView extends View {
   constructor() {
-    console.log(tag, 'constructor');
     super(qs('#search-form-view')); // View를 상속받아 this.element에 돔 요소를 추가
 
-    this.inputElement = qs('[type=text]', this.element);
+    this.inputElement = qs('[type=text]', this.element); // scope → 어떤 하위 요소인지
     this.resetElement = qs('[type=reset]', this.element);
+
     this.showResetButton(false);
     this.bindEvent();
   }
@@ -17,10 +19,28 @@ export default class SearchFormView extends View {
   }
 
   bindEvent() {
-    on(this.inputElement, 'keyup', () => this.handleKeyUp()); // 키보드에서 손을 땠을 때 발생하는 이벤트
+    on(this.inputElement, 'keyup', () => this.handleKeyUp());
+    on(this.element, 'submit', (e) => this.handleSubmit(e));
+    on(this.resetElement, 'click', () => this.handleReset());
   }
 
   handleKeyUp() {
-    console.log(tag, 'handleKeyUp');
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
+
+    if (value.length <= 0) {
+      this.handleReset();
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { value } = this.inputElement;
+    this.emit('@submit', { value }); // 커스텀 이벤트
+  }
+
+  handleReset() {
+    this.emit('@reset');
   }
 }
